13cf3e70ef97e8ecece9f8aac1eeb17504d37fb3
Maksym Liuban <Maksym.liuban@ideo-digital.com>
2022-01-30 23:11:40 +0200

    Add TournamentDetails & TournamentList templates

--- src/components/TournamentDetails.vue
+++ src/components/TournamentDetails.vue
@@ -1,26 +1,80 @@
 <template>
-    <div>
-        <p>{{tournament.title}}</p>
+  <span>
+    <header>
+      <img class="image img-fluid"  :src="'http://localhost:62542/' + tournament.imgUrl">
+    </header>
+    <div class="intro my-5 text-center">
+      <div class="container my-5">
+        <h3>{{tournament.title}}</h3>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h5>Date</h5>
+            <p class="details">{{tournament.date}}</p>
+          </div>
+          <div class="col">
+            <h5>Prize</h5>
+            <p class="details">${{tournament.prize}}</p>
+          </div>
+          <div class="col">
+            <h5>Winner</h5>
+            <p v-if="tournament.Winner != null" class="details">{{tournament.winner.firstName}} {{tournament.winner.lastName}}</p>
+            <p v-else class="details">To be determined</p>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="container col-md-7 mb-5">
+      <p>{{tournament.description}}</p>
+    </div>
+    <div class="container container col-md-7">
+      <table class="table table-borderless">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Participants</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="participant in tournament.participants" v-bind:key="participant.id">
+            <th scope="row">{{participant.id}}</th>
+            <td>
+              <a class="text-dark" href="/members/@part.Id">
+              {{participant.firstName}} {{participant.lastName}}</a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- <div class="secondaryImages container my-5">
+      @{ if(Model.SecondaryImages != null) { foreach(var image in
+      Model.SecondaryImages) {
+      <div><img class="mx-auto" src="@image.GetCropUrl("square")"></div>
+      } } }
+    </div> -->
+  </span>
 </template>
+
 
 <script>
 export default {
-    computed: {
-        tournament() {
-            return this.$store.state.currentTournament
-        }
+  computed: {
+    tournament() {
+      return this.$store.state.currentTournament;
     },
+  },
 
-    props: {
-        id: {
-            type: Number,
-            required: true
-        }
+  props: {
+    id: {
+      type: Number,
+      required: true,
     },
+  },
 
-    created() {
-        this.$store.dispatch('fetchTournamentById', this.id)
-    }
-}
+  created() {
+    this.$store.dispatch("fetchTournamentById", this.id);
+  },
+};
 </script>
--- src/components/TournamentList.vue
+++ src/components/TournamentList.vue
@@ -1,21 +1,43 @@
 <template>
-  <div>
-    <div v-for="tournament in tournaments" :key="tournament.title">
-      <router-link
-        :to="{ name: 'TournamentDetails', params: { id: tournament.id } }"
+  <div class="container mt-3">
+    <div class="row">
+      <div
+        class="col m-2"
+        v-for="tournament in tournaments"
+        :key="tournament.title"
       >
-        {{ tournament.title }}
-      </router-link>
-      <figure>
-        <router-link
+        <router-link class="link-dark text-decoration-none"
           :to="{ name: 'TournamentDetails', params: { id: tournament.id } }"
         >
-          <img :src="'http://localhost:62542/' + tournament.imgUrl" :alt="tournament.title" />
+          {{ tournament.title }}
         </router-link>
-      </figure>
+        <figure>
+          <router-link
+            :to="{ name: 'TournamentDetails', params: { id: tournament.id } }"
+          >
+            <img width="400px" height="250px"
+              :src="'http://localhost:62542/' + tournament.imgUrl"
+              :alt="tournament.title"
+            />
+          </router-link>
+        </figure>
+      </div>
     </div>
   </div>
 </template>
+<div class="container mt-3">
+    <div class="row">
+        @foreach(var item in Model.Children<Tournament>()) 
+        {
+        <div class="col m-2">
+            <a class="link-dark text-decoration-none" href="@item.Url">
+                <img width="400px" height="250px" src="@item.Image.Url" alt="@item.Title">
+                <p>@item.Title</p>
+            </a>
+        </div>
+        }
+    </div>
+</div>
 
 <script>
 export default {
