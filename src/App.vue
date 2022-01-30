938be0184b713883b288ed36073020c126ab8f1a
Maksym Liuban <Maksym.liuban@ideo-digital.com>
2022-01-30 22:39:38 +0200

    Install bootstrap & bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <TheNavigation/>
-    <hr>
     <router-view :key="$route.path"/>
   </div>
 </template>
@@ -26,12 +25,7 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
+/* @import'~bootstrap/dist/css/bootstrap.css' */
 </style>
--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,6 +1,32 @@
+<!--<template>
+  <div class="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/tournaments">Tournaments</router-link>
+  </div>
+</template> -->
+
 <template>
-    <div class="nav">
-        <router-link to="/">Home</router-link>
-        <router-link to="/tournaments">Tournaments</router-link>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Tennis</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/tournaments">Tournaments</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
 </template>
