2b652be76b1cd156ab8d21af1168c3235d630aed
Maksym Liuban <Maksym.liuban@ideo-digital.com>
2022-01-31 13:13:34 +0200

    Add fontawesome package

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import store from './store/index'
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrophy  } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTrophy)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 Vue.use(BootstrapVue);
 
