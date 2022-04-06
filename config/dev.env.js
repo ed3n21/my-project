9a4df2af0d2adff0fc6f7ed192377cc296fb9f5a
Maksim Liuban <maks.lyuban@gmail.com>
2022-04-06 13:07:57 +0300

    T-1: fix RTE field, add env variables for Umbraco9

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,11 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  ROOT_API: '"http://localhost:62542"',
-  TOURNAMENTS_API: '"http://localhost:62542/api/tournaments"',
-  PARTICIPANTS_API: '"http://localhost:62542/api/participants"'
+  ROOT_API: '"https://localhost:44342/"',
+  TOURNAMENTS_API: '"https://localhost:44342/api/tournaments"',
+  PARTICIPANTS_API: '"https://localhost:44342/api/participants"',
+  LOGIN_API: '"https://localhost:44342/login"',
+  ROOT_API_V8: '"http://localhost:62542"',
+  TOURNAMENTS_API_V8: '"http://localhost:62542/api/tournaments"',
+  PARTICIPANTS_API_V8: '"http://localhost:62542/api/participants"'
 })
