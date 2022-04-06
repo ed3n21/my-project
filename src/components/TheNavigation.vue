d705119a5fba73495f81a06c888ebcf463dfe232
Maksim Liuban <maks.lyuban@gmail.com>
2022-04-06 13:15:06 +0300

    authorization: add login button to navigation menu

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
   <b-navbar toggleable="lg" type="light" variant="light">
-    <b-navbar-brand href="/"><router-link class="nav-link text-dark" to="/">Tennis</router-link></b-navbar-brand>
+    <div class="container">
+      <b-navbar-brand href="/">
+      <router-link class="nav-link text-dark" to="/">Tennis</router-link>
+      </b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#"><router-link class="nav-link" to="/tournaments">Tournaments</router-link></b-nav-item>
-        <b-nav-item href="#"><router-link class="nav-link" to="/participants">Participants</router-link></b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="me-auto">
+          <b-nav-item>
+            <router-link class="nav-link" to="/tournaments">Tournaments</router-link>
+            </b-nav-item>
+          <b-nav-item>
+            <router-link class="nav-link" to="/participants">Participants</router-link>
+            </b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="nav-link" to="/login">Login</router-link>
+            </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </div>
   </b-navbar>
-</div>
-
 </template>
